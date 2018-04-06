a688a4f2da8a2b2c440b30366c6d19b841ac7e3a
Eko Rudiawan Jamzuri <eko.rudiawan@gmail.com>
2018-04-06 20:34:40 +0700

    Manually define PORTE register map to fix error PORTE, DDRE, and PINE undeclare for ATMega328P

--- firmware/src/main/hx711.h
+++ firmware/src/main/hx711.h
@@ -1,6 +1,12 @@
 #ifndef _HX_711_H
 #define _HX_711_H
 
+#define PORTE 	_SFR_IO8(0x0E)
+#define DDRE	_SFR_IO8(0x0D)
+#define PINE	_SFR_IO8(0x0C)
+#define PE2	2
+#define PE3	3
+
 #include <stdint.h>
 
 extern int32_t hx711_values[4];
